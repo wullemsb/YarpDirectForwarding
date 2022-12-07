03133b2d6e45f216df56eadf16d19fe6e93586f8
Wullems Bart <wullemsb@gmail.com>
2022-12-07 20:20:34 +0100

    Update example to align with original

---
+++ YarpDirectForwarding/CustomTransformer.cs
@@ -0,0 +1,39 @@
+﻿using Yarp.ReverseProxy.Forwarder;
+using Yarp.ReverseProxy.Transforms;
+
+namespace YarpDirectForwarding;
+
+/// <summary>
+/// Custom request transformation
+/// </summary>
+class CustomTransformer : HttpTransformer
+{
+    /// <summary>
+    /// A callback that is invoked prior to sending the proxied request. All HttpRequestMessage
+    /// fields are initialized except RequestUri, which will be initialized after the
+    /// callback if no value is provided. The string parameter represents the destination
+    /// URI prefix that should be used when constructing the RequestUri. The headers
+    /// are copied by the base implementation, excluding some protocol headers like HTTP/2
+    /// pseudo headers (":authority").
+    /// </summary>
+    /// <param name="httpContext">The incoming request.</param>
+    /// <param name="proxyRequest">The outgoing proxy request.</param>
+    /// <param name="destinationPrefix">The uri prefix for the selected destination server which can be used to create
+    /// the RequestUri.</param>
+    public override async ValueTask TransformRequestAsync(HttpContext httpContext, HttpRequestMessage proxyRequest, string destinationPrefix)
+    {
+        // Copy all request headers
+        await base.TransformRequestAsync(httpContext, proxyRequest, destinationPrefix);
+
+        // Customize the query string:
+        var queryContext = new QueryTransformContext(httpContext.Request);
+        queryContext.Collection.Remove("param1");
+        queryContext.Collection["area"] = "xx2";
+
+        // Assign the custom uri. Be careful about extra slashes when concatenating here. RequestUtilities.MakeDestinationAddress is a safe default.
+        proxyRequest.RequestUri = RequestUtilities.MakeDestinationAddress("https://example.com", httpContext.Request.Path, queryContext.QueryString);
+
+        // Suppress the original request header, use the one from the destination Uri.
+        proxyRequest.Headers.Host = null;
+    }
+}
--- YarpDirectForwarding/Program.cs
+++ YarpDirectForwarding/Program.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Net;
 using Yarp.ReverseProxy.Forwarder;
+using YarpDirectForwarding;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,10 +23,13 @@ var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
     ActivityHeadersPropagator = new ReverseProxyPropagator(DistributedContextPropagator.Current)
 });
 
+
+// Setup our own request transform class
+var transformer = new CustomTransformer(); // or HttpTransformer.Default;
 var requestConfig = new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromSeconds(100) };
 
 app.Map("/{**catch-all}", async (HttpContext httpContext, IHttpForwarder forwarder) => {
-    var error = await forwarder.SendAsync(httpContext, targetUrl, httpClient, requestConfig);
+    var error = await forwarder.SendAsync(httpContext, targetUrl, httpClient, requestConfig,transformer);
     // Check if the operation was successful
     if (error != ForwarderError.None)
     {
@@ -36,6 +40,9 @@ app.Map("/{**catch-all}", async (HttpContext httpContext, IHttpForwarder forward
 });
 
 app.Run();
+
+
+
 
 
 
