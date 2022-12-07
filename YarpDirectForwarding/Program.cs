a549530d53b2f33f216fecd475208b662c3efd0a
Wullems Bart <wullemsb@gmail.com>
2022-12-07 20:16:27 +0100

    Add project files.

---
+++ YarpDirectForwarding/Program.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.Net;
+using Yarp.ReverseProxy.Forwarder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var targetUrl = builder.Configuration.GetValue<string>("targetUrl");
+
+builder.Services.AddHttpForwarder();
+var app = builder.Build();
+
+app.UseRouting();
+
+//Always use HttpMessageInvoker rather than HttpClient, HttpClient buffers responses by default.
+//Buffering breaks streaming scenarios and increases memory usage and latency.
+var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
+{
+    UseProxy = false,
+    AllowAutoRedirect = false,
+    AutomaticDecompression = DecompressionMethods.None,
+    UseCookies = false,
+    ActivityHeadersPropagator = new ReverseProxyPropagator(DistributedContextPropagator.Current)
+});
+
+var requestConfig = new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromSeconds(100) };
+
+app.Map("/{**catch-all}", async (HttpContext httpContext, IHttpForwarder forwarder) => {
+    var error = await forwarder.SendAsync(httpContext, targetUrl, httpClient, requestConfig);
+    // Check if the operation was successful
+    if (error != ForwarderError.None)
+    {
+        var errorFeature = httpContext.GetForwarderErrorFeature();
+        var exception = errorFeature.Exception;
+        //TODO: Log exception
+    }
+});
+
+app.Run();
+
+
+
+
